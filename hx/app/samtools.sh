9e67e06c0a327ac18a85ce87f47ed5402a841f3f
yoshihikosuzuki <ys.neoteny@gmail.com>
2022-05-10 06:28:32 +0000

    add mods to hx

---
+++ hx/app/samtools.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+shopt -s expand_aliases
+source $HOME/.bashrc
+set -eux
+
+# DEFINE WHERE TO INSTALL, APP NAME AND VERSION
+MODROOT=/work/yoshihiko_s/app
+APP=samtools
+VER=1.15.1
+
+# MAKE THE MODULE DIRECTORY
+APPDIR=$MODROOT/$APP
+mkdir -p $APPDIR && cd $APPDIR
+
+# DOWNLOAD AND INSTALL TO `$APPDIR/$VER`
+wget -O - https://github.com/samtools/$APP/releases/download/$VER/$APP-$VER.tar.bz2 | tar xjvf -
+mkdir $VER && cd $APP-$VER
+./configure --prefix=$APPDIR/$VER && make && make install
+cd .. && rm -r $APP-$VER
+
+# WRITE A MODULEFILE
+cd $MODROOT/.modulefiles && mkdir -p $APP
+cat <<__END__ >$APP/$VER.lua
+-- Default settings
+local modroot    = "$MODROOT"
+local appname    = myModuleName()
+local appversion = myModuleVersion()
+local apphome    = pathJoin(modroot, myModuleFullName())
+
+-- Package settings
+prepend_path("PATH", pathJoin(apphome, "bin"))
+__END__
