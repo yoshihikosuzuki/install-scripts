ac4f2888d97a552941f0afcca9c2e4f810c9bdcc
yoshihikosuzuki <yoshi.suzuki.1992@gmail.com>
2023-10-16 16:01:09 +0900

    update

---
+++ deigo/unit-apps/minimap2.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -eu
+
+MODROOT=/hpcshare/appsunit/MyersU
+APP=minimap2
+VER=2.24
+
+APPDIR=$MODROOT/$APP
+mkdir -p $APPDIR && cd $APPDIR
+
+wget -O - https://github.com/lh3/minimap2/archive/refs/tags/v$VER.tar.gz | tar xzvf -
+mv $APP-$VER $VER
+cd $VER && make
+
+cd $MODROOT/.modulefiles && mkdir -p $APP
+cat <<__END__ >$APP/$VER.lua
+-- Default settings
+local modroot    = "$MODROOT"
+local appname    = myModuleName()
+local appversion = myModuleVersion()
+local apphome    = pathJoin(modroot, myModuleFullName())
+
+-- Package settings
+depends_on("Other/k8")
+prepend_path("PATH", apphome)
+prepend_path("PATH", pathJoin(apphome, "misc"))
+__END__
