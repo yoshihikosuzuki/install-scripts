8765f7b7e2e9fb7392cbcbf0b7925444bb3a9efa
yoshihikosuzuki <yoshi.suzuki.1992@gmail.com>
2024-02-08 14:01:58 +0900

    add to mdx

---
+++ mdx/plotsr.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+module purge
+set -eux
+
+# DEFINE WHERE TO INSTALL, APP NAME AND VERSION
+MODROOT=/large/yoshihiko_s/app
+APP=plotsr
+VER=1.1.1
+
+# MAKE THE MODULE DIRECTORY
+APPDIR=$MODROOT/$APP
+mkdir -p $APPDIR && cd $APPDIR
+
+# DOWNLOAD AND INSTALL TO `$APPDIR/$VER`
+CONDA_SH=Miniconda3-py38_23.11.0-2-Linux-x86_64.sh
+curl -O https://repo.anaconda.com/miniconda/${CONDA_SH}
+bash ${CONDA_SH} -b -p $APPDIR/$VER
+rm ${CONDA_SH}
+cd $VER
+./bin/conda install -c conda-forge -c defaults -c bioconda -y $APP=$VER
+rm -rf pkgs
+
+# WRITE A MODULEFILE
+cd $MODROOT/.modulefiles && mkdir -p $APP
+cat <<__END__ >$APP/$VER.lua
+-- Default settings
+local modroot    = "$MODROOT"
+local appname    = myModuleName()
+local appversion = myModuleVersion()
+local apphome    = pathJoin(modroot, myModuleFullName())
+
+-- Package settings
+prepend_path("PATH", pathJoin(apphome, "bin"))
+__END__
