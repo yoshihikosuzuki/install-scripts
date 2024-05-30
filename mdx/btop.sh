0e0227a09a24fd3ab4ebf82907b14ab09a832fb0
yoshihikosuzuki <yoshi.suzuki.1992@gmail.com>
2024-05-30 12:05:39 +0900

    update

---
+++ mdx/btop.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+module purge
+set -eux
+
+# DEFINE WHERE TO INSTALL, APP NAME AND VERSION
+MODROOT=/large/yoshihiko_s/app
+APP=btop
+VER=1.3.2
+
+# MAKE THE MODULE DIRECTORY
+APPDIR=$MODROOT/$APP
+mkdir -p $APPDIR && cd $APPDIR
+
+# DOWNLOAD AND INSTALL TO `$APPDIR/$VER`
+wget -O - https://github.com/aristocratos/btop/releases/download/v${VER}/btop-x86_64-linux-musl.tbz | tar xjvf -
+mv btop ${VER}
+
+# WRITE A MODULEFILE
+cd $MODROOT/.modulefiles && mkdir -p $APP
+cat <<__END__ >$APP/$VER.lua
+-- Default settings
+local modroot    = "$MODROOT"
+local appname    = myModuleName()
+local appversion = myModuleVersion()
+local apphome    = pathJoin(modroot, myModuleFullName())
+
+-- Package settings
+prepend_path("PATH", pathJoin(apphome, "bin"))
+__END__
