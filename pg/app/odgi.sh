33dfc0a53063fc31479b2a6b8c88f20cd7c315ef
yoshihikosuzuki <yoshi.suzuki.1992@gmail.com>
2024-06-06 11:17:28 +0900

    update

---
+++ pg/app/odgi.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+module purge
+set -eux
+
+# DEFINE WHERE TO INSTALL, APP NAME AND VERSION
+MODROOT=/nfs/data05/yoshihiko_s/app
+APP=odgi
+VER=0.8.6
+
+# MAKE THE MODULE DIRECTORY
+APPDIR=$MODROOT/$APP
+mkdir -p $APPDIR && cd $APPDIR
+
+# DOWNLOAD AND INSTALL TO `$APPDIR/$VER`
+mkdir -p $VER
+cd $VER
+singularity pull $APP.sif docker://pangenome/$APP:1713304113
+for CMD in odgi; do
+    echo '#!/bin/sh' >$CMD
+    echo "singularity exec $APPDIR/$VER/$APP.sif $CMD \$*" >>$CMD
+    chmod +x $CMD
+done
+
+# WRITE A MODULEFILE
+cd $MODROOT/.modulefiles && mkdir -p $APP
+cat <<__END__ >$APP/$VER.lua
+-- Default settings
+local modroot    = "$MODROOT"
+local appname    = myModuleName()
+local appversion = myModuleVersion()
+local apphome    = pathJoin(modroot, myModuleFullName())
+
+-- Package settings
+prepend_path("PATH", apphome)
+unsetenv("PERL5LIB")
+setenv("PERL_BADLANG", "0")
+setenv("SINGULARITY_BIND", "/data,/glusterfs,/glusterfs2,/glusterfs3,/grid2,/hpgdata,/hpgwork,/hpgwork2,/nfs/data05,/nfs/data06,/nfs/data07,/nfs/data08")
+__END__
