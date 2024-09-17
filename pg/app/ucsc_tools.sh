fd43ad2f8d9ba6cf71ff66537bbdb12d29e34c86
yoshihikosuzuki <yoshi.suzuki.1992@gmail.com>
2024-09-17 13:25:26 +0900

    update

---
+++ pg/app/ucsc_tools.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+module purge
+set -eux
+
+# DEFINE WHERE TO INSTALL, APP NAME AND VERSION
+MODROOT=/nfs/data05/yoshihiko_s/app
+APP=ucsc_tools
+VER=2024.09.13
+
+# MAKE THE MODULE DIRECTORY
+APPDIR=$MODROOT/$APP
+mkdir -p $APPDIR && cd $APPDIR
+
+# DOWNLOAD AND INSTALL TO `$APPDIR/$VER`
+# NOTE: https://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64
+# NOTE: copied from mdx
+# rsync -aP rsync://hgdownload.soe.ucsc.edu/genome/admin/exe/linux.x86_64/ ./$VER
+
+# WRITE A MODULEFILE
+cd $MODROOT/.modulefiles && mkdir -p $APP
+cat <<__END__ >$APP/$VER.lua
+-- Default settings
+local modroot    = "$MODROOT"
+local appname    = myModuleName()
+local appversion = myModuleVersion()
+local apphome    = pathJoin(modroot, myModuleFullName())
+
+-- Package settings
+prepend_path("PATH", apphome)
+__END__
