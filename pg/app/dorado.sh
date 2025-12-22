91c22c5d0985e62185277a5736588110b2ded9c6
yoshihikosuzuki <yoshi.suzuki.1992@gmail.com>
2025-12-22 17:59:48 +0900

    add

--- pg/app/dorado.sh
+++ pg/app/dorado.sh
@@ -5,26 +5,29 @@ set -eux
 # DEFINE WHERE TO INSTALL, APP NAME AND VERSION
 MODROOT=/nfs/data05/yoshihiko_s/app
 APP=dorado
-VER=0.7.2
+VER=1.2.0
 
 # MAKE THE MODULE DIRECTORY
 APPDIR=$MODROOT/$APP
 mkdir -p $APPDIR && cd $APPDIR
 
-# DOWNLOAD AND INSTALL TO `$APPDIR/$VER`
+## precompiled binary (with GLIBC requirements)
 wget -O - https://cdn.oxfordnanoportal.com/software/analysis/dorado-${VER}-linux-x64.tar.gz | tar zxvf -
 mv dorado-${VER}-linux-x64 $VER
+cd $VER
 
-# NOTE: download models
-#       `$ dorado basecaller ${DORADO_MODEL_DIR}/dna_r10.4.1_e8.2_400bps_${MODE}@v${MODEL_VER} ... (--modified-bases 5mCG_5hmCG)`
+## Download models
 mkdir -p model
 cd model
-MODEL_VER=4.3.0
+MODEL_VER=5.2.0
+MOD_MODEL_VER=2
 for MODE in fast hac sup; do
     wget https://cdn.oxfordnanoportal.com/software/analysis/dorado/dna_r10.4.1_e8.2_400bps_${MODE}@v${MODEL_VER}.zip
     unzip dna_r10.4.1_e8.2_400bps_${MODE}@v${MODEL_VER}.zip
-    wget https://cdn.oxfordnanoportal.com/software/analysis/dorado/dna_r10.4.1_e8.2_400bps_${MODE}@v${MODEL_VER}_5mCG_5hmCG@v1.zip
-    unzip dna_r10.4.1_e8.2_400bps_${MODE}@v${MODEL_VER}_5mCG_5hmCG@v1.zip
+done
+for MODE in hac sup; do
+    wget https://cdn.oxfordnanoportal.com/software/analysis/dorado/dna_r10.4.1_e8.2_400bps_${MODE}@v${MODEL_VER}_5mCG_5hmCG@v${MOD_MODEL_VER}.zip
+    unzip dna_r10.4.1_e8.2_400bps_${MODE}@v${MODEL_VER}_5mCG_5hmCG@v${MOD_MODEL_VER}.zip
 done
 cd ..
 
---
+++ pg/app/pbccs.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+module purge
+set -eux
+
+MODROOT=/nfs/data05/yoshihiko_s/app
+APP=pbccs
+VER=6.4.0
+
+APPDIR=$MODROOT/$APP
+mkdir -p $APPDIR && cd $APPDIR
+
+mkdir -p $VER
+cd $VER
+wget -O - https://github.com/PacificBiosciences/ccs/releases/download/v${VER}/ccs.tar.gz | tar xzvf -
+chmod +x ccs
+
+cd $MODROOT/.modulefiles && mkdir -p $APP
+cat <<__END__ >$APP/$VER.lua
+-- Default settings
+local modroot    = "$MODROOT"
+local appname    = myModuleName()
+local appversion = myModuleVersion()
+local apphome    = pathJoin(modroot, myModuleFullName())
+
+-- Package settings
+prepend_path("PATH", apphome)
+__END__
